b2066b687ac5bb495abf0f032ba7cd9c1b6cda17
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 21:46:00 +0530

    f

--- src/Components/IntroContainer/IntroContainer.styled.jsx
+++ src/Components/IntroContainer/IntroContainer.styled.jsx
@@ -1,15 +1,34 @@
 import styled from "styled-components";
+import { DEFAULT_COLOURS } from "../../base/default.colors";
 
 export const IntroContainer = styled.div`
   width: 100%;
-  height: 700px;
+  padding: 0px 20px;
+  height: 580px;
 `;
 
 export const Content = styled.div`
   width: 50%;
   height: 100%;
+  padding-right: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: 10px;
   h1 {
-    font-size: 50px;
+    font-size: 70px;
     margin-bottom: 0px;
+    margin-top: 100px;
+    line-height: 0.9em;
+    span {
+      color: ${DEFAULT_COLOURS.primaryOptional2};
+    }
+  }
+  h2 {
+    margin-bottom: 0px;
+    margin-top: 5px;
+  }
+  p {
+    font-size: 18px;
   }
 `;
