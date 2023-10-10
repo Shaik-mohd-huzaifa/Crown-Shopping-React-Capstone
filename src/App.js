6ee466f267841a75f09d014c02323f39c1bbd321
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 10:26:23 +0530

    Add catch error statement

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import { setCurrentUser } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
 
 import "./App.css";
-import { Routes, Route } from "react-router";
+import { Routes, Route, redirect } from "react-router";
 import { CategoryContainer } from "./Routes/Categories-Menu/category-container";
 import { Navbar } from "./Routes/Navigation/navbar.component";
 import Signin from "./Routes/Authentication/authentication.component";
 import Shop from "./Routes/Shop/shop.component";
 import Checkout from "./Routes/Checkout/Checkout.component";
+import { Error404 } from "./Components/404/404";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = onAuthUserStateChanged((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
+      try {
+        if (user) {
+          createUserDocumentFromAuth(user);
+        }
+      } catch (error) {
+        dispatch(setCurrentUser({}));
+        redirect("/404");
       }
       dispatch(setCurrentUser(user));
     });
@@ -32,6 +38,7 @@ function App() {
           <Route index element={<CategoryContainer />} />
           <Route path="shop/*" element={<Shop />}></Route>
           <Route path="auth" element={<Signin />} />
+          <Route path="404" element={<Error404 />} />
           <Route path="checkout" element={<Checkout />} />
         </Route>
       </Routes>
