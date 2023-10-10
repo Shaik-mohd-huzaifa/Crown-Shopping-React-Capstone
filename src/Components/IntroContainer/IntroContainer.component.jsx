d38b06358f8aa374389ed338bd8f130ca063828d
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 20:57:38 +0530

    Add header and description

--- src/Components/IntroContainer/IntroContainer.component.jsx
+++ src/Components/IntroContainer/IntroContainer.component.jsx
@@ -1,7 +1,20 @@
-import { IntroContainer } from "./IntroContainer.styled";
+import { Content, IntroContainer } from "./IntroContainer.styled";
 
 const Intro = () => {
-  return <IntroContainer></IntroContainer>;
+  return (
+    <IntroContainer>
+      <Content>
+        <h1>Welcome to Couture Cart</h1>
+        <h2>Elevate Your Style with Couture Elegance</h2>
+        <p>
+          Couture Cart is where fashion meets sophistication. Discover a curated
+          selection of high-end clothing and accessories designed to redefine
+          elegance. Unveil a world of fashion that resonates with your unique
+          taste and sets you apart from the crowd.
+        </p>
+      </Content>
+    </IntroContainer>
+  );
 };
 
 export default Intro;
