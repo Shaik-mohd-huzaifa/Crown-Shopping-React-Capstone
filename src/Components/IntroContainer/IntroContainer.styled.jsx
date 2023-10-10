66994f6a676a910b7bf2b6f3afc2d9fd0e3d79cd
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 20:59:21 +0530

    Removed the margin bottom from header

--- src/Components/IntroContainer/IntroContainer.styled.jsx
+++ src/Components/IntroContainer/IntroContainer.styled.jsx
@@ -4,3 +4,12 @@ export const IntroContainer = styled.div`
   width: 100%;
   height: 700px;
 `;
+
+export const Content = styled.div`
+  width: 50%;
+  height: 100%;
+  h1 {
+    font-size: 50px;
+    margin-bottom: 0px;
+  }
+`;
