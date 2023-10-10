d4ab3f1cdccc703c6576666528dd343e1274a079
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 18:01:38 +0530

    Imported Intro into Category Container

--- src/Routes/Categories-Menu/category-container.jsx
+++ src/Routes/Categories-Menu/category-container.jsx
@@ -2,10 +2,12 @@ import "./categories.style.scss";
 import CategoryCard from "../../Components/Category-container/category-card.component";
 import categoriesData from "./categories.json";
 import { Outlet } from "react-router";
+import Intro from "../../Components/IntroContainer/IntroContainer.component";
 
 export const CategoryContainer = () => {
   return (
     <div>
+      <Intro />
       <div className="categories-container">
         {categoriesData.map((category) => (
           <CategoryCard
