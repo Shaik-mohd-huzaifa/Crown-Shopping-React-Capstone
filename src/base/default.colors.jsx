f03c53f0b8da5449ee4aebcd6693145528224fb6
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-10 20:13:21 +0530

    Moved base outside the directory and changed the scss to jsx and wrapped the colors in js object

---
+++ src/base/default.colors.jsx
@@ -0,0 +1,8 @@
+export const DEFAULT_COLOURS = {
+  primary: "#600881",
+  secondary: "#ed47b1",
+  primaryOptional: "#f59a8e",
+  primaryOptional2: "#6a55b8",
+  primaryOptional3: "#862cbb",
+  gradient: "linear-gradient(45deg, $primaryOptional2, $primaryOptional3)",
+};
