901f719143b691504af88a301145e5510b26d46f
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-11 15:13:23 +0530

    Changed the Color of primaryOptional2

--- src/base/default.colors.jsx
+++ src/base/default.colors.jsx
@@ -2,7 +2,7 @@ export const DEFAULT_COLOURS = {
   primary: "#600881",
   secondary: "#ed47b1",
   primaryOptional: "#f59a8e",
-  primaryOptional2: "#6a55b8",
+  primaryOptional2: "#8253fb",
   primaryOptional3: "#862cbb",
   gradient: "linear-gradient(45deg, $primaryOptional2, $primaryOptional3)",
 };
