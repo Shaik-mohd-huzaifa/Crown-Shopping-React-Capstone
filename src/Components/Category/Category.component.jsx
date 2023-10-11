02208b1ebaa4a81c07cf2b1f711404abb5559ca3
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-11 21:17:05 +0530

    center header

--- src/Components/Category/Category.component.jsx
+++ src/Components/Category/Category.component.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { ShopdataContext } from "../../contexts/ShopData.Context";
 import { ProductCard } from "../Product-Card/Product-card.component";
-// import "./Category.styles.scss";
+import "./Category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
