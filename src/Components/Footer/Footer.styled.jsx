2fd5dd1887a1c88394f1ed952ec3c899486054b4
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-11 18:34:05 +0530

    SHOPSASS015

--- src/Components/Footer/Footer.styled.jsx
+++ src/Components/Footer/Footer.styled.jsx
@@ -3,7 +3,6 @@ import { DEFAULT_COLOURS } from "../../base/default.colors";
 
 export const FooterContainer = styled.div`
   width: 100%;
-  padding: 10px;
   border-radius: 10px;
   background-color: ${DEFAULT_COLOURS.primaryOptional2};
 `;
