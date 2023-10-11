a37226f90ddd2420a7f89d318f50c14e7c5b8738
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-10-11 18:34:42 +0530

    SHOPSASS016

--- src/Components/Footer/Footer.styled.jsx
+++ src/Components/Footer/Footer.styled.jsx
@@ -20,6 +20,7 @@ export const FooterBottom = styled.div`
   display: flex;
   justify-content: space-between;
   border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 `;
 
 export const BrandInfo = styled.div``;
